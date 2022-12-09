795a825793fd967444d2326b00444f67cc799fc7
KennethKo <kennethko+github@gmail.com>
2022-12-09 12:36:57 -0800

    prettier and version bump

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -5,80 +5,90 @@ test('Jest heartbeat - trivial test', () => {})
 const zeroPhrase = 'AbackAbackAardvark'
 
 test('randomFriendlyPhrase happy path', () => {
-    expect(randomFriendlyPhrase()).toBeDefined()
-    expect(randomFriendlyPhrase({})).toBeDefined()
-    expect(randomFriendlyPhrase()).not.toEqual(zeroPhrase)
+  expect(randomFriendlyPhrase()).toBeDefined()
+  expect(randomFriendlyPhrase({})).toBeDefined()
+  expect(randomFriendlyPhrase()).not.toEqual(zeroPhrase)
 })
 
 test('randomFriendlyPhrase form', () => {
-    expect(randomFriendlyPhrase({
-        form: ['object'],
-        delimeter: ' ',
-    }).split(' ').length).toEqual(1)
-    expect(randomFriendlyPhrase({
-        form: ['predicate','object','team','collection','predicate','object','team','collection'],
-        delimeter: ' ',
-    }).split(' ').length).toEqual(8)
-    expect(randomFriendlyPhrase({
-        form: ['predicate','object','team','collection','foo'],
-        delimeter: ',,',
-    }).split(',,').length).toEqual(4)
+  expect(
+    randomFriendlyPhrase({
+      form: ['object'],
+      delimeter: ' ',
+    }).split(' ').length,
+  ).toEqual(1)
+  expect(
+    randomFriendlyPhrase({
+      form: ['predicate', 'object', 'team', 'collection', 'predicate', 'object', 'team', 'collection'],
+      delimeter: ' ',
+    }).split(' ').length,
+  ).toEqual(8)
+  expect(
+    randomFriendlyPhrase({
+      form: ['predicate', 'object', 'team', 'collection', 'foo'],
+      delimeter: ',,',
+    }).split(',,').length,
+  ).toEqual(4)
 })
 
 test('randomFriendlyPhrase form error', () => {
-    expect(() => randomFriendlyPhrase({form: []})).toThrowError(/InvalidArgumentError: illegal form.*/)
-    expect(() => randomFriendlyPhrase({form: ['foo', 'bar']})).toThrowError(/InvalidArgumentError: illegal form.*/)
+  expect(() => randomFriendlyPhrase({ form: [] })).toThrowError(/InvalidArgumentError: illegal form.*/)
+  expect(() => randomFriendlyPhrase({ form: ['foo', 'bar'] })).toThrowError(/InvalidArgumentError: illegal form.*/)
 })
 
 test('randomFriendlyPhrase prefix', () => {
-    expect(randomFriendlyPhrase({prefix: 'D'})).toMatch(/D.*/)
-    expect(randomFriendlyPhrase({prefix: 'qu'})).toMatch(/Qu.*/)
-    expect(randomFriendlyPhrase({form: ['object'], prefix: 'Z'})).toMatch(/Z.*/)
-    expect(randomFriendlyPhrase({form: ['object'], prefix: 'Z', delimeter: ',,'}).split(',,').length).toEqual(1)
+  expect(randomFriendlyPhrase({ prefix: 'D' })).toMatch(/D.*/)
+  expect(randomFriendlyPhrase({ prefix: 'qu' })).toMatch(/Qu.*/)
+  expect(randomFriendlyPhrase({ form: ['object'], prefix: 'Z' })).toMatch(/Z.*/)
+  expect(randomFriendlyPhrase({ form: ['object'], prefix: 'Z', delimeter: ',,' }).split(',,').length).toEqual(1)
 })
 
 test('randomFriendlyPhrase prefix error', () => {
-    expect(() => randomFriendlyPhrase({prefix: 'aa'})).toThrowError(/d/)
-    expect(randomFriendlyPhrase({prefix: 'aa', form: ['object', 'predicate']})).toMatch(/Aa.*/)
+  expect(() => randomFriendlyPhrase({ prefix: 'aa' })).toThrowError(/d/)
+  expect(randomFriendlyPhrase({ prefix: 'aa', form: ['object', 'predicate'] })).toMatch(/Aa.*/)
 })
 
 const code = 'foobar'
 const codePhrase = 'SameHeadyPeridot'
 test('randomFriendlyPhrase code', () => {
-    expect(randomFriendlyPhrase({code})).toMatch(codePhrase)
-    expect(randomFriendlyPhrase({code})).toMatch(randomFriendlyPhrase({code}))
-    expect(randomFriendlyPhrase({code})).not.toMatch(randomFriendlyPhrase({code: code+'baz'}))
-    // terms should never equal
-    const [pred1, pred2] = randomFriendlyPhrase({code: code+'baz', form: ['predicate', 'predicate'], delimeter: ','}).split(',')
-    expect(pred1).not.toEqual(pred2)
+  expect(randomFriendlyPhrase({ code })).toMatch(codePhrase)
+  expect(randomFriendlyPhrase({ code })).toMatch(randomFriendlyPhrase({ code }))
+  expect(randomFriendlyPhrase({ code })).not.toMatch(randomFriendlyPhrase({ code: code + 'baz' }))
+  // terms should never equal
+  const [pred1, pred2] = randomFriendlyPhrase({
+    code: code + 'baz',
+    form: ['predicate', 'predicate'],
+    delimeter: ',',
+  }).split(',')
+  expect(pred1).not.toEqual(pred2)
 })
 
 const seed = '0'
 test('randomFriendlyPhrase seed', () => {
-    expect(randomFriendlyPhrase({seed})).toMatch('LapisLikeStretch')
-    expect(randomFriendlyPhrase({seed, code})).toMatch(codePhrase)
-    expect(randomFriendlyPhrase({seed})).toMatch('TricolorHillTable')
-    expect(randomFriendlyPhrase({seed})).toMatch('TenderCapableJonquil')
-    expect(randomFriendlyPhrase({seed: '1'})).toMatch('OssifiedDaffyArtichoke')
-    expect(randomFriendlyPhrase({seed})).toMatch('OpaqueCubicCreek')
+  expect(randomFriendlyPhrase({ seed })).toMatch('LapisLikeStretch')
+  expect(randomFriendlyPhrase({ seed, code })).toMatch(codePhrase)
+  expect(randomFriendlyPhrase({ seed })).toMatch('TricolorHillTable')
+  expect(randomFriendlyPhrase({ seed })).toMatch('TenderCapableJonquil')
+  expect(randomFriendlyPhrase({ seed: '1' })).toMatch('OssifiedDaffyArtichoke')
+  expect(randomFriendlyPhrase({ seed })).toMatch('OpaqueCubicCreek')
 
-    const phrases = [
-        'LapisLikeStretch',
-        'OssifiedDaffyArtichoke',    // seed 0 and 1 are tested above
-        'PicturesqueEightAmbulance',
-        'ScintillatingMulberryList',
-        'ThinkableFlutteringPatella',
-        'CeruleanThankfulDiploma',
-        'SaltySteadfastBillboard',
-        'GlamorousRippleOkra',
-        'InvitedTorchAddress',
-        'SleetPuzzledTangelo',
-    ]
-    for (let i=2; i<10; i++) {
-        expect(randomFriendlyPhrase({seed: `${i}`})).toEqual(phrases[i])
-    }
-    expect(() => randomFriendlyPhrase({seed: '10'})).toThrowError(/IllegalStateError: cannot generate for seed 10.*/)
-    // seed remains disregarded when code is passed
-    expect(randomFriendlyPhrase({seed: '10', code})).toMatch(codePhrase)
-    expect(() => randomFriendlyPhrase({seed: '10'})).toThrowError(/IllegalStateError: cannot generate for seed 10.*/)
+  const phrases = [
+    'LapisLikeStretch',
+    'OssifiedDaffyArtichoke', // seed 0 and 1 are tested above
+    'PicturesqueEightAmbulance',
+    'ScintillatingMulberryList',
+    'ThinkableFlutteringPatella',
+    'CeruleanThankfulDiploma',
+    'SaltySteadfastBillboard',
+    'GlamorousRippleOkra',
+    'InvitedTorchAddress',
+    'SleetPuzzledTangelo',
+  ]
+  for (let i = 2; i < 10; i++) {
+    expect(randomFriendlyPhrase({ seed: `${i}` })).toEqual(phrases[i])
+  }
+  expect(() => randomFriendlyPhrase({ seed: '10' })).toThrowError(/IllegalStateError: cannot generate for seed 10.*/)
+  // seed remains disregarded when code is passed
+  expect(randomFriendlyPhrase({ seed: '10', code })).toMatch(codePhrase)
+  expect(() => randomFriendlyPhrase({ seed: '10' })).toThrowError(/IllegalStateError: cannot generate for seed 10.*/)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,24 @@
-
-import {objects, predicates, teams, collections} from 'friendly-words'
+import { objects, predicates, teams, collections } from 'friendly-words'
 import * as gen from 'random-seed'
 
-const friendlyWords: {[key: string]: string[]} = { objects, predicates, teams, collections }
+const friendlyWords: { [key: string]: string[] } = { objects, predicates, teams, collections }
 
 type Opts = {
-    // types
-    form?: string[]
-    delimeter?: string
-    prefix?: string
-    code?: string
-    seed?: string
+  // types
+  form?: string[]
+  delimeter?: string
+  prefix?: string
+  code?: string
+  seed?: string
 }
 
 /**
- * Returns randomized, human-readable, worksafe phrase. e.g. 'TremendousSpangleFreezer' 
- * 
+ * Returns randomized, human-readable, worksafe phrase. e.g. 'TremendousSpangleFreezer'
+ *
  * Useful for temporary user names or transient room names.
  * All options below are optional with good defaults.
- * 
- * @param {string[]} [opts.form = ['predicate', 'predicate', 'object']] An array of keys describing the form of the desired phrase. 
+ *
+ * @param {string[]} [opts.form = ['predicate', 'predicate', 'object']] An array of keys describing the form of the desired phrase.
  *  Keys must be selected from the following word group keys with the following cardinalities:
  *   predicate - 1454
  *   object - 3072
@@ -29,76 +28,75 @@ type Opts = {
  *  Illegal keys are ignored.
  *  The default form (['predicate', 'predicate', 'object']) has a possibility space of roughly 10bn.
  * @param {string} [opts.delimeter = ''] The delimeter between each word in the phrase. Default empty string.
- * @param {string} [opts.prefix = undefined] If defined, constrains the space of the first word in the phrase. 
+ * @param {string} [opts.prefix = undefined] If defined, constrains the space of the first word in the phrase.
  *   Case insensitive.
  * @param {string} [ops.code = undefined] If defined, a code that should always map to the same stable phrase.
  *   This should be stable across versions, as we're locked to v1.2.0 of friendly-words.
  *   This can map identifiers to stable friendly phrases.
- * @param {string} [ops.seed = undefined] If defined, defines a stable seed for producing an identical series of 
- *   random phrases from initialization. 
+ * @param {string} [ops.seed = undefined] If defined, defines a stable seed for producing an identical series of
+ *   random phrases from initialization.
  *   Call this function repeatedly with the same seed to make it behave like a generator along the same sequence.
- *   Intended for stable unit testing or for stable internal startup. 
+ *   Intended for stable unit testing or for stable internal startup.
  *   This should be stable across versions, as we're locked to v1.2.0 of friendly-words.
  *   Not intended for general use. Throws if more than 10 different seeds are ever requested.
  *   Ignored if ops.code is defined.
  */
-function randomFriendlyPhrase(opts : Opts = {}) {
-    // named parameter defaults
-    const {
-        form = ['predicate', 'predicate', 'object'],
-        delimeter = '',
-        prefix,
-    } = opts
-    opts = {form, delimeter, prefix, ...opts}
+function randomFriendlyPhrase(opts: Opts = {}) {
+  // named parameter defaults
+  const { form = ['predicate', 'predicate', 'object'], delimeter = '', prefix } = opts
+  opts = { form, delimeter, prefix, ...opts }
 
-    const normForm = form.map(k => k+'s').filter(k => !!friendlyWords[k])
-    if (!normForm.length) throw Error(`InvalidArgumentError: illegal form [${form}]`)
+  const normForm = form.map((k) => k + 's').filter((k) => !!friendlyWords[k])
+  if (!normForm.length) throw Error(`InvalidArgumentError: illegal form [${form}]`)
 
-    const picker = getPicker(opts)
+  const picker = getPicker(opts)
 
-    return normForm.map((k, i) => {
-        let space = friendlyWords[k]
-        if (i === 0 && prefix) {
-            const lowPrefix = prefix.toLowerCase()
-            space = friendlyWords[k].filter((w:string) => w.startsWith(lowPrefix))
-            if (!space.length) 
-                throw Error(`InvalidArgumentError: could not find prefix '${prefix}' in word group '${k.substring(0, k.length-1)}'`)
-        }
-        return space[picker.range(space.length)]
+  return normForm
+    .map((k, i) => {
+      let space = friendlyWords[k]
+      if (i === 0 && prefix) {
+        const lowPrefix = prefix.toLowerCase()
+        space = friendlyWords[k].filter((w: string) => w.startsWith(lowPrefix))
+        if (!space.length)
+          throw Error(
+            `InvalidArgumentError: could not find prefix '${prefix}' in word group '${k.substring(0, k.length - 1)}'`,
+          )
+      }
+      return space[picker.range(space.length)]
     })
     .map(capitalize)
     .join(delimeter)
 }
 
 // gen.create()'s gen.RandomSeed fulfills the generalized local Picker type
-type Picker = {range: (range: number) => number}
+type Picker = { range: (range: number) => number }
 const rand = gen.create()
-const rands: {[key: string]: Picker} = {}
+const rands: { [key: string]: Picker } = {}
 
-const getPicker = ({code, seed} : Opts) : Picker => {
-    // let code return a stable, unique pick with an avalanche effect
-    if (code != null) {
-        // picker lifecycle within phrase
-        return gen.create(code)
+const getPicker = ({ code, seed }: Opts): Picker => {
+  // let code return a stable, unique pick with an avalanche effect
+  if (code != null) {
+    // picker lifecycle within phrase
+    return gen.create(code)
+  }
+  // let seed return a stable random sequence
+  if (seed != null) {
+    if (rands[seed] != null) {
+      return rands[seed]
+    } else {
+      if (Object.keys(rands).length >= 10)
+        throw Error(
+          `IllegalStateError: cannot generate for seed ${seed} when 10 other seeds have already been initialized`,
+        )
+      // picker lifecycle across phrases. Adding salt to make seed and code differ
+      rands[seed] = gen.create(seed + 'Salt')
+      return rands[seed]
     }
-    // let seed return a stable random sequence
-    if (seed != null) {
-        if (rands[seed] != null) {
-            return rands[seed]
-        } else {
-            if (Object.keys(rands).length >= 10)
-                throw Error(`IllegalStateError: cannot generate for seed ${seed} when 10 other seeds have already been initialized`)
-            // picker lifecycle across phrases. Adding salt to make seed and code differ
-            rands[seed] = gen.create(seed+'Salt')
-            return rands[seed]
-        }
-    }
-    // otherwise return an unbiased randomizer
-    return rand
+  }
+  // otherwise return an unbiased randomizer
+  return rand
 }
 
 const capitalize = (s: string) => s[0].toUpperCase() + s.slice(1)
 
-
 export default randomFriendlyPhrase
-
